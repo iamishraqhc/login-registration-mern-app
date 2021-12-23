9f85a01061bece69229a03b321f28ebd57b89017
Ishraq Haider Chowdhury <ishraq-haider.chowdhury@stud.uni-bamberg.de>
2021-12-23 01:31:37 +0100

    created and styled Registration form

--- frontend/src/Pages/Register.js
+++ frontend/src/Pages/Register.js
@@ -1,9 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useHistory } from 'react-router-dom'
+import axios from 'axios'
 
 const Register = () => {
+    const history = useHistory()
+
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const registerUser = async (event) => {
+        event.preventDefault()
+
+        const response = await axios.get('https://localhost:5000/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                name,
+                email,
+                password
+            })
+        })
+
+        const data = await response.data
+
+        if (data.status === 'OK') {
+            history.push('/login')
+        }
+    }
+
     return (
         <div>
-            
+            <h1>Register</h1>
+            <form onSubmit={registerUser}>
+                <input 
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    type="text"
+                    placeholder="Name"
+                />
+                <input 
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    type="email"
+                    placeholder="Email"
+                />
+                <input 
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    type="password"
+                    placeholder="Password"
+                />
+                <input type="submit" value="Register" />
+            </form>
         </div>
     )
 }
